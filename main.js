599cfaa734f7b8e75c88c6742e910b5f1e9b6b55
lubin123 <chenbinlu6@gmail.com>
2025-11-15 21:09:58 +0800

    light dark mode

--- main.js
+++ main.js
@@ -440,3 +440,23 @@ function displayError(message) {
     materialList.innerHTML = `<tr><td colspan="6">${message}</td></tr>`;
   }
 }
+
+let lightmode = localStorage.getItem("lightmode");
+const toggleDayNight = document.getElementById("switch-theme");
+
+const enableLightMode = () => {
+  document.body.classList.add("lightmode");
+  localStorage.setItem("lightmode", "active");
+};
+
+const disableLightMode = () => {
+  document.body.classList.remove("lightmode");
+  localStorage.setItem("lightmode", null);
+};
+
+if (lightmode === "active") enableLightMode();
+
+toggleDayNight.addEventListener("click", () => {
+  lightmode = localStorage.getItem("lightmode");
+  lightmode !== "active" ? enableLightMode() : disableLightMode();
+});
